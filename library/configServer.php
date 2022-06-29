ea5293140563ef507edfeaceb646cc79c7d67e2a
Anderson <andersondavidpc@ufps.edu.co>
2022-06-29 11:47:56 -0500

    Corrección servidor

--- library/configServer.php
+++ library/configServer.php
@@ -1,5 +1,5 @@
 <?php
 define("USER", "khziwpjqwl");
-define("SERVER", "lilith-store-server");
+define("SERVER", "lilith-store-server.mysql.database.azure.com");
 define("BD", "lilith-store-database");
 define("PASS", "A6452I4A4KDHZ5OQ$");
